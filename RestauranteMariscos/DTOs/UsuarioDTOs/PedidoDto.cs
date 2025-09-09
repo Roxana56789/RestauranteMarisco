c62a74de8aea30ab8b0aeeb1d48cb5fa99363628
Jeannette Hernández <ch1234536@outlook.es>
2025-09-09 14:46:12 -0600

    se eliminaron los atributos de pedidosDto

--- RestauranteMariscos/DTOs/UsuarioDTOs/PedidoDto.cs
+++ RestauranteMariscos/DTOs/UsuarioDTOs/PedidoDto.cs
@@ -1,10 +1,6 @@
 ﻿namespace RestauranteMariscos.DTOs.UsuarioDTOs
 {
     public class PedidoDto
-    {
-        public int Id { get; set; }
-        public DateTime Fecha { get; set; }
-        public decimal Total { get; set; }
-        public int UsuarioId { get; set; }      
+    {   
     }
 }
